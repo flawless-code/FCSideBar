0e6b279f52fe01573331a2c8b1b33a5451c5e484
Kavinkumar <kavinkumar@mallow-tech.com>
2020-09-26 16:09:14 +0530

    Initial Commit

---
+++ FCSideBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FCSideBar
+//
+//  Created by Kavinkumar Veerakumar on 26/09/20.
+//  Copyright © 2020 Kavinkumar Veerakumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
